d0936dec201040e82c981cac82c5b388035a581b
Akshith Sai Kondamadugu <akshithsai24@gmail.com>
2024-10-23 14:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from flask import Flask, request, jsonify,render_template
+import pandas as pd
 import pickle
 
 app = Flask(__name__)
-
+app.static_folder = 'static'
 @app.route('/')
-
 def home():
+    return render_template('welcome.html')
+
+
+@app.route('/startprediction',methods=['POST'])
+def startprediction():
     return render_template('home.html')
+
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    model = pickle.load(open("model.pkl","rb"))
-    a = [int(x) for x in request.form.values()]
-    pp = model.predict_proba([a])
-    p = model.predict([a])
-    
-    return render_template('p.html',p_text=f"prediction = {p} and probability = {pp[0,0]}")
+    #model = pickle.load(open("model.pkl","rb"))
+    b = request.form.to_dict()
+    print(b)
+    b = pd.DataFrame(b,index=[0])
+
+    #a = [float(x) for x in request.form.values()]
+
+    trans = pickle.load(open("data_x.pkl","rb"))
+    b = trans.transform(b)
+    model = pickle.load(open("model.pkl","rb")) 
+    p = model.predict(b)
+    return render_template('p.html',p_text = f"Prediction = {p[0]}  ")
 
 
 
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
-
