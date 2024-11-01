908b5464ef5d4a46f52a7f346aae4efe73990f90
Akshith Sai Kondamadugu <akshithsai24@gmail.com>
2024-11-01 10:19:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify,render_template
 import pandas as pd
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 app.static_folder = 'static'
+filename = 'heart-disease-prediction-knn-model.pkl'
+model = pickle.load(open(filename, 'rb'))
 @app.route('/')
 def home():
     return render_template('welcome.html')
@@ -13,20 +16,35 @@ def home():
 def startprediction():
     return render_template('home.html')
 
-
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    #model = pickle.load(open("model.pkl","rb"))
-    b = request.form.to_dict()
-    print(b)
-    
-    model = pickle.load(open("model.pkl","rb")) 
-    p = model.predict(b)
-    return render_template('p.html',p_text = f"Prediction = {p[0]}  ")
+    if request.method == 'POST':
+
+        age = int(request.form['age'])
+        sex = request.form.get('sex')
+        cp = request.form.get('cp')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = model.predict(data)
+        
+        return render_template('p.html', prediction=my_prediction)
+
 
 
 
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#<script src="script.js"></script>
