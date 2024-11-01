3104d57422157c143d6c2e6017c8b813bfaee04b
Akshith Sai Kondamadugu <akshithsai24@gmail.com>
2024-11-01 10:18:56 +0530

    Update ml_code.py

--- ml_code.py
+++ ml_code.py
@@ -1,33 +1,57 @@
+# importing required libraries
+import numpy as np
 import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-import numpy as np
-import pickle
-
-df = pd.read_csv('framingham.csv')
-df.drop(['education'], inplace = True, axis = 1)
-df.rename(columns ={'male':'Sex_male'}, inplace = True)
-df.dropna(axis = 0, inplace = True)
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
+# loading and reading the dataset
 
-X = np.asarray(df[['age', 'Sex_male', 'cigsPerDay','totChol', 'sysBP','BMI','heartRate','glucose']])
+heart = pd.read_csv("heart_cleveland_upload.csv")
 
-y = np.asarray(df['TenYearCHD'])
+# creating a copy of dataset so that will not affect our original dataset.
+heart_df = heart.copy()
 
+# Renaming some of the columns 
+heart_df = heart_df.rename(columns={'condition':'target'})
+print(heart_df.head())
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# model building 
 
-# Train logistic regression model
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+#fixing our data in x and y. Here y contains target data and X contains rest all the features.
+x= heart_df.drop(columns= 'target')
+y= heart_df.target
 
+# splitting our dataset into training and testing for this we will use train_test_split library.
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=42)
 
-# Make predictions
-y_pred = model.predict(X_test)
-# Evaluate model
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Model Accuracy: {accuracy:.2f}')
+#feature scaling
+scaler= StandardScaler()
+x_train_scaler= scaler.fit_transform(x_train)
+x_test_scaler= scaler.fit_transform(x_test)
 
-pickle.dump(model,open("model.pkl","wb"))            # pkl for pickle extension ---- to read : m = pickle.load(open("model.pkl","rb"))
+# creating K-Nearest-Neighbor classifier
+model=RandomForestClassifier(n_estimators=20)
+model.fit(x_train_scaler, y_train)
+y_pred= model.predict(x_test_scaler)
+p = model.score(x_test_scaler,y_test)
+print(p)
+
+print('Classification Report\n', classification_report(y_test, y_pred))
+print('Accuracy: {}%\n'.format(round((accuracy_score(y_test, y_pred)*100),2)))
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# Creating a pickle file for the classifier
+filename = 'heart-disease-prediction-knn-model.pkl'
+pickle.dump(model, open(filename, 'wb'))
+
